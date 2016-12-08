39d63bf2550bfc33449c9e1a6f9965d1f0aa50a0
meowsqueak <meowsqueak@email>
2016-12-08 13:37:31 +1300

    Day 1.

---
+++ 1b.py
@@ -0,0 +1,32 @@
+input_data = "R5, L2, L1, R1, R3, R3, L3, R3, R4, L2, R4, L4, R4, R3, L2, L1, L1, R2, R4, R4, L4, R3, L2, R1, L4, R1, R3, L5, L4, L5, R3, L3, L1, L1, R4, R2, R2, L1, L4, R191, R5, L2, R46, R3, L1, R74, L2, R2, R187, R3, R4, R1, L4, L4, L2, R4, L5, R4, R3, L2, L1, R3, R3, R3, R1, R1, L4, R4, R1, R5, R2, R1, R3, L4, L2, L2, R1, L3, R1, R3, L5, L3, R5, R3, R4, L1, R3, R2, R1, R2, L4, L1, L1, R3, L3, R4, L2, L4, L5, L5, L4, R2, R5, L4, R4, L2, R3, L4, L3, L5, R5, L4, L2, R3, R5, R5, L1, L4, R3, L1, R2, L5, L1, R4, L1, R5, R1, L4, L4, L4, R4, R3, L5, R1, L3, R4, R3, L2, L1, R1, R2, R2, R2, L1, L1, L2, L5, L3, L1"
+
+def main(d):
+    dir = 0
+    x, y = 0, 0
+    locations = {}
+    for move in d.replace(" ","").split(","):
+        if move[0] == "R":
+            dir+=1
+        else:
+            dir+=3
+        dir %= 4
+        dist = int(move[1:])
+        for i in range(dist):
+            if dir == 0:
+                y += 1
+            elif dir == 2:
+                y -= 1
+            elif dir == 1:
+                x += 1
+            else:
+                x -= 1
+            visits = locations.get((x,y), 0)
+            if visits == 1:
+                print(x, y, abs(x) + abs(y))
+            locations[(x, y)] = visits + 1
+
+    print x, y, abs(x) + abs(y)
+
+if __name__ == "__main__":
+    main(input_data)
+
